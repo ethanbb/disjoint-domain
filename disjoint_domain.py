6024bc52c078e1cab1832fdaf107bfae7ddbb0d8
Ethan Blackwood <ethanbblackwood@gmail.com>
2020-10-23 21:23:03 -0400

    Get rumelnet working and add code for disjoint domain

--- disjoint_domain.py
+++ disjoint_domain.py
@@ -4,21 +4,23 @@ from scipy.cluster import hierarchy
 from scipy.spatial import distance
 import matplotlib.pyplot as plt
 
+N_ITEMS = 8
 
-def make_attr_vecs(n_contexts=4, attrs_per_context=50, rng_seed=None):
+
+def _make_attr_vecs(n_contexts, attrs_per_context, rng):
     """
     Make some attribute vectors that conform to the Euclidean distance plot (Figure R3, bottom).
-    There are 8 items. Outputs an (8 * n_contexts) x (n_contexts * attrs_per_context) matrix, which is
-    block-diagonal with block size n_contexts x n_contexts. These attributes are simply repeated for each domain.
+    There are 8 items. Outputs a list of n_contexts 8 x attrs_per_context matrices.
+    These attributes are simply repeated for each domain.
 
     Input attrs_per_context must be at least 50 (approximately) in order for the distances to be correct.
     Each vector has 25 attributes activated within the current context, so that cross-context distances are sqrt(50).
     """
 
-    rng = np.random.default_rng(seed=rng_seed)
+    if rng is None:
+        rng = np.random.default_rng()
 
-    n_items = 8
-    attrs = [np.zeros((n_items, attrs_per_context), dtype='int32') for _ in range(n_contexts)]
+    attrs = [np.zeros((N_ITEMS, attrs_per_context), dtype='int32') for _ in range(n_contexts)]
 
     for kC in range(n_contexts):
         # first, handle items 1-4, which all pairwise have distance 2, i.e. each pair differs by 4 bits.
@@ -74,41 +76,55 @@ def make_attr_vecs(n_contexts=4, attrs_per_context=50, rng_seed=None):
     return attrs
 
 
-def plot_item_attributes(attrs):
+def make_io_mats(n_contexts=4, attrs_per_context=50, rng=None, n_domains=4):
+    """Make the actual item, context, and attribute matrices, across a given number of domains."""
+
+    # First make it for a single domain, then use block_diag to replicate.
+    item_mat_1 = np.tile(np.eye(N_ITEMS), (n_contexts, 1))
+    context_mat_1 = np.repeat(np.eye(n_contexts), N_ITEMS, axis=0)
+    attrs = _make_attr_vecs(n_contexts, attrs_per_context, rng)
+    attr_mat_1 = block_diag(*attrs)
+
+    item_mat = block_diag(*[item_mat_1 for _ in range(n_domains)])
+    context_mat = block_diag(*[context_mat_1 for _ in range(n_domains)])
+    attr_mat = block_diag(*[attr_mat_1 for _ in range(n_domains)])
+
+    return item_mat, context_mat, attr_mat
+
+
+def plot_item_attributes(n_contexts=4, attrs_per_context=50, rng=None):
     """Item and context inputs and attribute outputs for each input combination (regardless of domain)"""
 
-    n_items = 8
-    n_ctx = len(attrs)
+    item_mat, context_mat, attr_mat = make_io_mats(n_contexts, attrs_per_context, rng, n_domains=1)
 
     fig = plt.figure()
 
     # plot 1: items
     ax = fig.add_subplot(1, 20, (1, 3))
     ax.set_title('Items')
-    item_mat = np.tile(np.eye(n_items), (n_ctx, 1))
     ax.imshow(item_mat, aspect='auto', interpolation='nearest')
     ax.set_yticks([])
-    ax.set_xticks(range(0, n_items, 2))
+    ax.set_xticks(range(0, N_ITEMS, 2))
 
     # plot 2: contexts
     ax = fig.add_subplot(1, 20, 4)
     ax.set_title('Contexts')
-    context_mat = np.repeat(np.eye(n_ctx), n_items, axis=0)
     ax.imshow(context_mat, aspect='auto', interpolation='nearest')
     ax.set_yticks([])
-    ax.set_xticks(range(0, n_ctx, 2))
+    ax.set_xticks(range(0, context_mat.shape[1], 2))
 
     # plot 3: attributes
     ax = fig.add_subplot(1, 20, (5, 20))
     ax.set_title('Attributes')
-    attr_mat = block_diag(*attrs)
     ax.imshow(attr_mat, aspect='auto', interpolation='nearest')
     ax.set_yticks([])
     ax.set_xticks(range(0, attr_mat.shape[1], 10))
 
 
-def plot_item_attribute_dendroram(attrs):
-    """Dendrogram of similarities between the items' attributes, collapsed across contexts (regardless of domain)"""
+def plot_item_attribute_dendroram(n_contexts=4, attrs_per_context=50, rng=None):
+    """Dendrogram of similarities between the items' attributes, collapsed across contexts"""
+
+    attrs = _make_attr_vecs(n_contexts, attrs_per_context, rng)
 
     fig, ax = plt.subplots()
     mean_dist = np.mean(np.stack([distance.pdist(a) for a in attrs]), axis=0)
